7f40b938ee4b498b19be8508ac50abac42c3298a
wangcham <651122857@qq.com>
2023-12-04 20:04:32 +0800

    功能一能用

--- lab2/client/src/main.rs
+++ lab2/client/src/main.rs
@@ -1,12 +1,12 @@
-use std::{io::{self, Write, stdout, Read}, net::{TcpListener, TcpStream}, fs::File};
+use std::{io::{self, Write, Read}, net::TcpStream, fs::File};
 
-fn main(){
+fn main() {
     println!("请输入选项");
     println!("输入1为获取大写字符串");
     println!("输入2为发送文件");
     println!("输入quit退出程序");
-    
-    loop{
+
+    loop {
         print!(" >>> ");
         io::stdout().flush().unwrap();
         let mut input = String::new();
@@ -22,82 +22,84 @@ fn main(){
             }
         }
     }
-
 }
 
-fn str_method(){
+fn str_method() {
     println!("请输入字符串,按回车输入完成");
     io::stdout().flush().unwrap();
     let mut strinput = String::new();
     io::stdin().read_line(&mut strinput).unwrap();
     let input = strinput.trim();
     let flag = 1;
-    let info = send(& input,flag);
+    let info = send(input, flag);
     match info {
-        Some(value) => println!("{}",value),
+        Some(value) => println!("{}", value),
         None => println!("连接失败"),
     }
 }
 
-fn file_method(){
-    println!("请输入文件名字:");
+fn file_method() {
+    println!("请输入文件地址:");
     io::stdout().flush().unwrap();
     let mut strinput = String::new();
     io::stdin().read_line(&mut strinput).unwrap();
     let input = strinput.trim();
     let flag = 2;
-    let info = send(&input,flag);
+    let info = send(input, flag);
     print!("{}", flag);
     match info {
-        Some(value) => println!("{}",value),
+        Some(value) => println!("{}", value),
         None => println!("连接失败"),
     }
 }
 
-fn send(str:&str,flag:i32) -> Option<String>{
-    println!("请输入服务器ip地址和端口");
-    println!("比如127.0.0.1:5000");
-    io::stdout().flush().unwrap();
-    let mut strinput = String::new();
-    io::stdin().read_line(&mut strinput).unwrap();
-    let addr = strinput.trim();
-    let addr = addr.trim();
-    
-    //判断是否连接成功
-    let mut stream =match TcpStream::connect(addr){
+fn send(str: &str, flag: i32) -> Option<String> {
+    println!("设置地址为本地5000端口");
+    let mut addr = "127.0.0.1:5000";
+
+    let mut stream = match TcpStream::connect(addr) {
         Ok(stream) => {
             println!("连接成功");
             stream
         }
         Err(_) => return None,
     };
-    println!("{}",flag);
-    if flag == 1 {
-        let filename = str.trim();
-        let content = String::from("1")+filename;
-        stream.write_all(content.as_bytes()).unwrap();
-
-        stream.flush().unwrap();
+    if flag == 1{
+        // 使用 Vec<u8> 作为缓冲区
         let mut buffer = Vec::new();
+        let data = flag.to_string()+ &str + "9";
+        println!("{}",&str);
+        buffer.extend_from_slice(data.as_bytes());
+        stream.write_all(&buffer).unwrap();
 
-        stream.read_to_end(&mut buffer).unwrap();
-        //判断文件是否存在，依据是接收的流是否为N
-        if String::from_utf8_lossy(&mut buffer) == "N" {
-            return Some("未查询到文件".to_string());
+        let mut sx = String::new();
+
+        // 循环读取数据，直到遇到结束符 "9"
+        loop {
+            let mut temp_buffer = [0; 1]; // 临时缓冲区，用于读取一个字符
+            match stream.read(&mut temp_buffer) {
+                Ok(n) => {
+                    if n == 0 { // 如果读取到的数据长度为 0，表示连接已经关闭
+                        break;
+                    }
+                    let temp_s = String::from_utf8_lossy(&temp_buffer);
+                    if temp_s == "9" { // 如果读取到的字符是结束符 "9"
+                        break;
+                    } else {
+                        sx.push_str(&temp_s);
+                    }
+                }
+                Err(_) => {
+                    println!("读取数据时发生错误");
+                    break;
+                }
+            }
         }
-        //接受文件
-        let mut file = File::create("received_file.txt").unwrap();
-        file.write_all(&buffer).unwrap();
-        return Some("接收到文件，名字是received_file".to_string());
-
+        let real  = &sx[1..];
+        return Some(real.to_string());
+    
     }else{
-        let str = String::from("2")+str;
-        stream.write_all(str.as_bytes()).unwrap();
-        stream.flush().unwrap();
-        let mut buffer = Vec::new();
-        stream.read_to_end(&mut buffer).unwrap();
-        let received_string = String::from_utf8_lossy(&buffer);
-        
-        return Some(received_string.to_string());
+        let mut buffer = 
+
     }
 }
--- lab2/server/src/main.rs
+++ lab2/server/src/main.rs
@@ -1,44 +1,62 @@
 use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, fs::File};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:5000");
-    match listener {
-        Ok(listener) => {
-            println!("监听成功！");
-            for stream in listener.incoming() {
-                let stream = stream.unwrap();
-                handle_stream(stream);
-            }
-        },
-        Err(e) => panic!("监听失败：{}", e),  
-    }
+    let listener = TcpListener::bind("127.0.0.1:5000").expect("监听失败");
 
+    println!("监听成功！");
+
+    for stream in listener.incoming() {
+        let stream = stream.expect("获取客户端连接时发生错误");
+        handle_stream(stream);
+    }
 }
 
-fn handle_stream(mut stream:TcpStream){
-    println!("进入handle——stream");
-    let mut buffer = Vec::new();
-    stream.read_to_end(&mut buffer).unwrap();
-    let s = String::from_utf8_lossy(&buffer);
-    let first_char = s.chars().next().unwrap();
-    println!("{}",first_char);
-    let str:String = s.chars().skip(1).collect();
+fn handle_stream(mut stream: TcpStream) {
+    let mut s = String::new();
+    let mut buffer = [0; 1]; // Use a single-element buffer
 
-    if first_char == '1' {
-
-        match File::open(&str) {
-            Ok(mut file) => {
-                let mut contents = Vec::new();
-                file.read_to_end(&mut contents).unwrap();
-                stream.write_all(&contents).unwrap();
-            }
-            Err(_) => {
-                stream.write_all(b"N").unwrap();
+loop {
+    match stream.read_exact(&mut buffer) {
+        Ok(_) => {
+            let temps = String::from_utf8_lossy(&buffer);
+            if temps == "9" {
+                break;
+            } else {
+                s.push_str(&temps);
             }
         }
-    }else{
-        let uppercase = str.to_uppercase();
-        stream.write_all(uppercase.as_bytes()).unwrap();
+        Err(_) => {
+            println!("读取数据失败");
+            break;
+        }
     }
+}
 
+    let first_char = s.chars().next().expect("解析字符串失败");
+    println!("first char is {}", first_char);
+
+    let str_content: String = s.chars().skip(1).collect();
+
+    // 处理接收到的数据
+    if first_char == '2' {
+        println!("进入2方法");
+        match File::open(&str_content) {
+            Ok(mut file) => {
+                let mut contents = Vec::new();
+                file.read_to_end(&mut contents).expect("读取文件内容时发生错误");
+                stream.write_all(&contents).expect("向客户端写入文件内容时发生错误");
+            }
+            Err(_) => {
+                stream.write_all(b"N").expect("向客户端写入错误标志时发生错误");
+            }
+        }
+    } else {
+        println!("获取字符串操作");
+        let one = "1";
+        let uppercase = format!("{}{}{}", one, str_content.to_uppercase(), "9");
+        let response = uppercase.as_bytes();
+
+        // 发送数据
+        stream.write_all(response).unwrap();
+    }
 }
