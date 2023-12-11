d8386ae31b6a3e8b1f2d37dbae135d2527059fe9
wangcham <651122857@qq.com>
2023-12-11 23:32:51 +0800

    lab2完成

--- lab2/client/src/main.rs
+++ lab2/client/src/main.rs
@@ -25,7 +25,7 @@ fn main() {
 }
 
 fn str_method() {
-    println!("请输入字符串,按回车输入完成");
+    println!("请输入内容，按回车输入完成");
     io::stdout().flush().unwrap();
     let mut strinput = String::new();
     io::stdin().read_line(&mut strinput).unwrap();
@@ -44,6 +44,7 @@ fn file_method() {
     let mut strinput = String::new();
     io::stdin().read_line(&mut strinput).unwrap();
     let input = strinput.trim();
+    println!("filepath is {}",input);
     let flag = 2;
     let info = send(input, flag);
     print!("{}", flag);
@@ -99,7 +100,29 @@ fn send(str: &str, flag: i32) -> Option<String> {
         return Some(real.to_string());
     
     }else{
-        let mut buffer = 
+        let mut buffer = Vec::new();
+        let data = flag.to_string()+&str+"9";
+        //仍然以9作为结尾，标志流的末尾
+        buffer.extend_from_slice(data.as_bytes());
+        stream.write_all(&buffer).unwrap();
+        print!("flag is {}",flag);
+        
+    let mut buffer = [0; 512]; // 创建一个缓冲区来存储接收到的数据
+    let mut file = File::create("received_file").unwrap(); // 创建一个文件来保存接收到的数据
+
+    loop {
+        let nbytes = stream.read(&mut buffer).unwrap(); // 从流中读取数据
+        if nbytes == 0 {
+            break; // 如果没有读取到数据，那么就跳出循环
+        }
+        file.write_all(&buffer[..nbytes]).unwrap(); // 将数据写入文件
+    }
+
+    println!("文件已接收并保存到本地");
+
+    return Some(String::from("文件传输成功"));
+
+
 
     }
 }
--- lab2/server/src/main.rs
+++ lab2/server/src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, fs::File};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, fs::{File, self}, num::ParseIntError};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:5000").expect("监听失败");
@@ -40,16 +40,7 @@ loop {
     // 处理接收到的数据
     if first_char == '2' {
         println!("进入2方法");
-        match File::open(&str_content) {
-            Ok(mut file) => {
-                let mut contents = Vec::new();
-                file.read_to_end(&mut contents).expect("读取文件内容时发生错误");
-                stream.write_all(&contents).expect("向客户端写入文件内容时发生错误");
-            }
-            Err(_) => {
-                stream.write_all(b"N").expect("向客户端写入错误标志时发生错误");
-            }
-        }
+        send_file(&str_content,&mut stream);
     } else {
         println!("获取字符串操作");
         let one = "1";
@@ -58,5 +49,20 @@ loop {
 
         // 发送数据
         stream.write_all(response).unwrap();
+    }
+}
+
+fn send_file(file_path: &str, stream: &mut TcpStream) {
+    println!("filepath is {}", file_path);
+    match File::open(file_path) {
+        Ok(mut file) => {
+            let contents = fs::read(file_path).expect("无法读取文件");
+            stream.write_all(&contents).expect("发送失败");
+            println!("发送成功");
+        }
+        Err(_) => {
+            // 发送错误标志
+            stream.write_all(b"N").expect("发送错误标志失败");
+        }
     }
 }
